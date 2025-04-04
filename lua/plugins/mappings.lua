b80f3b30cdc73c7857b68f99e7cee41cfb18c009
Bruno Vicenzo <bruno@alumni.usp.br>
2025-04-04 12:37:58 -0300

    Add ,nt mapping

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -11,6 +11,7 @@ return {
           ["<Leader>jt"] = { "<esc>:%!ruby -rjson -e \"print JSON.pretty_generate(JSON.parse(ARGF.read))\"<esc>=%<cr>", desc = "Intent JSON file" },
           ["<Leader>bd"] = { "<esc>:bw<cr>", desc = "Closes current buffer" },
           ["<Leader>,"] = { "<esc>:e#<cr>", desc = "Goes to last opened buffer" },
+          ["<Leader>nt"] = { "<esc>:Neotree toggle<cr>", desc = "Goes to last opened buffer" },
         },
         t = {
           -- setting a mapping to false will disable it
